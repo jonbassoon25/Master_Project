5cfb207d5dba22cab673aaeeecfef08dff4964b6
Jonathan Hanson <jonbassoon25@gmail.com>
2025-09-29 14:11:21 -0700

    Update ultrasonic.m

--- ultrasonic.m
+++ ultrasonic.m
@@ -1,1 +1,10 @@
 brick = ConnectBrick('PHIL');
+
+while (true)
+    distance = brick.UltrasonicDist(2);
+    fprintf("%d cm\n", distance);
+
+    if (distance < 25)
+        brick.playTone(500, 1);
+    end
+end
