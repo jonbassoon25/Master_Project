ddb4a57452b955a030f32085ada8dcd25c213f9d
lmcamp12 <lmcamp12@asu.edu>
2025-09-29 14:23:27 -0700

    Update colorSensor.m

--- colorSensor.m
+++ colorSensor.m
@@ -1,5 +1,12 @@
 brick.SetColorMode(3, 2);
 color = brick.ColorCode(3);
-while color 
-if color = 5
-  stop(1)
+while color ~= 3
+  if color == 5
+    stop(1)
+  end
+  if color == 4
+    manuel
+  end
+  if color == 2
+    manuel
+  end
