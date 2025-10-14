40c8f72e512576386ba7b184c79c4e615f474404
Jonathan Hanson <jonbassoon25@gmail.com>
2025-10-13 19:16:33 -0700

    Add comments, property typing, & argument typing to the ManualController class

--- MATLAB/Controllers/ManualController.m
+++ MATLAB/Controllers/ManualController.m
@@ -1,23 +1,28 @@
 classdef ManualController < handle
-    % A manual controller class for the car
+    % Controls the provided DriveTrain given keyboard input
+    properties (Access = protected, Constant)
+        FORWARD_ACCELERATION double = 1.0; % The forward acceleration constant in cm/s²
+        ANGULAR_ACCELERATION double = 1.0; % The angular acceleration constant in deg/s² counter clockwise
+    end
+
 
     properties (Access = protected)
-        keyboard
-        driveTrain
-        forwardAcceleration
-        angularAcceleration
-        targetForwardVelocity
-        targetAngularVelocity
+        keyboard Keyboard            % The Keyboard to detect user input from
+        driveTrain DriveTrain        % The DriveTrain to send control output to
+        targetForwardVelocity double % The current target forward velocity in cm/s
+        targetAngularVelocity double % The current target angular velocity in deg/s counter clockwise
     end
 
 
     methods (Access = public)
         function controller = ManualController(keyboard, driveTrain)
-            % Static values
-            controller.forwardAcceleration = 1; % Must be > 0. find actual value
-            controller.angularAcceleration = 1; % Must be > 0. find actual value
+            % Constructor
+            arguments
+                keyboard Keyboard     % The Keyboard to detect user input from
+                driveTrain DriveTrain % The DriveTrain to send control output to
+            end
 
-            % Initialize values
+            % Initialize class properties
             controller.keyboard = keyboard;
             controller.driveTrain = driveTrain;
             controller.targetForwardVelocity = 0.0;
@@ -25,49 +30,52 @@ classdef ManualController < handle
         end
 
         function Reset(controller)
+            % Resets the control output variables & stops the controlled DriveTrain
+            arguments
+                controller DriveTrain % This DriveTrain Object
+            end
+
             controller.targetForwardVelocity = 0.0;
             controller.targetAngularVelocity = 0.0;
             controller.driveTrain.Stop();
         end
 
+       
         function Update(controller)
-            % Updates the car's motion based on the keyboard input
-            if (isPressed("w"))
-                controller.targetForwardVelocity = controller.targetForwardVelocity + controller.forwardAcceleration;
-            elseif (isPressed("s"))
-                controller.targetForwardVelocity = controller.targetForwardVelocity - controller.forwardAcceleration;
-            else
-                if (controller.targetForwardVelocity < 0)
-                    controller.targetForwardVelocity = controller.targetForwardVelocity + controller.forwardAcceleration;
-                    if (controller.targetForwardVelocity > 0)
-                        controller.targetForwardVelocity = 0;
-                    end
-                else
-                    controller.targetForwardVelocity = controller.targetForwardVelocity - controller.forwardAcceleration;
-                    if (controller.targetForwardVelocity < 0)
-                        controller.targetForwardVelocity = 0;
-                    end
-                end
+            % Updates the control output to the DriveTrain based on keyboard input
+            arguments
+                controller DriveTrain % This DriveTrain Object
             end
 
-            if (isPressed("a"))
-                controller.targetAngularVelocity = controller.targetAngularVelocity + controller.angularAcceleration;
-            elseif (isPressed("d"))
-                controller.targetAngularVelocity = controller.targetAngularVelocity - controller.angularAcceleration;
-            else
-                if (controller.targetAngularVelocity < 0)
-                    controller.targetAngularVelocity = controller.targetAngularVelocity + controller.angularAcceleration;
-                    if (controller.targetAngularVelocity > 0)
-                        controller.targetAngularVelocity = 0;
-                    end
-                else
-                    controller.targetAngularVelocity = controller.targetAngularVelocity - controller.angularAcceleration;
-                    if (controller.targetAngularVelocity < 0)
-                        controller.targetAngularVelocity = 0;
-                    end
-                end
+            fowardInput = isPressed("w");
+            backwardInput = isPressed("s");
+            leftInput = isPressed("a");
+            rightInput = isPressed("d");
+            
+            
+            if (fowardInput) % Forward Input
+                controller.targetForwardVelocity = controller.targetForwardVelocity + controller.FORWARD_ACCELERATION;
             end
-                
+            if (backwardInput) % Backward Input
+                controller.targetForwardVelocity = controller.targetForwardVelocity - controller.FORWARD_ACCELERATION;
+            end
+            if (leftInput) % Left Input
+                controller.targetAngularVelocity = controller.targetAngularVelocity + controller.ANGULAR_ACCELERATION;
+            end
+            if (rightInput) % Right Input
+                controller.targetAngularVelocity = controller.targetAngularVelocity - controller.ANGULAR_ACCELERATION;
+            end
+
+            % No Forward or Backward Input
+            if (~(fowardInput || backwardInput)) 
+                controller.targetForwardVelocity = 0;
+            end 
+            % No Left or Right Input
+            if (~(leftInput || rightInput))
+                controller.targetAngularVelocity = 0;
+            end
+   
+            % Set & Manage DriveTrain Movement Targets
             controller.driveTrain.SetMixedMovementTargets(controller.targetForwardVelocity, controller.targetAngularVelocity);
             controller.driveTrain.ManageVelocityTargets();
         end
