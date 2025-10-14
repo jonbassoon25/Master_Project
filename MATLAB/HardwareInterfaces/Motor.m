65dc261ca0b923446965735d2299563bf6743a9e
Jonathan Hanson <jonbassoon25@gmail.com>
2025-10-13 21:41:17 -0700

    Add Motor documentation

--- MATLAB/HardwareInterfaces/Motor.m
+++ MATLAB/HardwareInterfaces/Motor.m
@@ -1,33 +1,33 @@
 classdef Motor < handle
-    % A motor connected to a brick
+    % A Motor object represents a motor connected to an EV3 brick
+
+    properties (Constant, Access = protected)
+        ANGLE_PID PID = PID(0.005, 255, 5);           % The angle PID controller for this motor
+        VELOCITY_PID PID = PID(0.00031, 80, 0.01325); % The velocity PID controller for this motor
+    end
 
     properties (Access = protected)
-        % Hardware values
-        brick
-        port
-
-        % Current motor data
-        currentAngle
-        currentVelocity
-        currentAcceleration
-        measurementClock
-
-        % Current Movement Mode
-        %   0 for none, 1 for angle, 2 for velocity
-        movementMode
-
-        % Movement Targets
-        angleTarget
-        velocityTarget
-
-        % Movement PIDs
-        anglePID
-        velocityPID
+        brick Brick                % The EV3 brick that this motor is connected to
+        port string                % The motor port letter that this motor is connected to
+        currentAngle double        % The current angle of this motor in degrees
+        currentVelocity double     % The current velocity of this motor in deg/sec
+        currentAcceleration double % The current acceleration of this motor in deg/sec²
+        measurementClock           % The clock recording the time since the last update cycle
+        movementMode uint8         % The current movement mode of this motor. 0 for none, 1 for angle, 2 for velocity
+        angleTarget double         % The target angle of this motor in degrees
+        velocityTarget double      % The target velocity of this motor 
     end
 
     methods (Access = public)
         function motor = Motor(brick, port)
-            % Constructs a new motor object.
+            % Initializes the properties of a new Motor object
+            arguments (Input)
+                brick Brick % The EV3 brick that this motor is connected to
+                port string % The motor port letter that this motor is connected to
+            end
+            arguments (Output)
+                motor Motor % The constructed Motor object
+            end
 
             % Set hardware values
             motor.port = port;
@@ -37,23 +37,24 @@ classdef Motor < handle
             motor.currentAngle = 0;
             motor.currentVelocity = 0;
             motor.currentAcceleration = 0;
-
             motor.measurementClock = tic;
+            
+            % Populate the data values
+            for i = 0:2
+                motor.UpdateData();
+            end
 
-            motor.UpdateData();
-
-            % Set initial targets & movement mode
-            motor.angleTarget = [];
-            motor.velocityTarget = [];
+            % Set the initial targets & movement mode
+            motor.angleTarget = 0.0;
+            motor.velocityTarget = 0.0;
             motor.movementMode = 0;
-
-            % Initialize PID controller variables
-            motor.anglePID = PID(0.005, 255, 5);
-            motor.velocityPID = PID(0.00031, 80, 0.01325);
         end
 
         function UpdateData(motor)
             % Updates the current motor data & active PID
+            arguments (Input)
+                motor Motor % This Motor object
+            end
             deltaTime = toc(motor.measurementClock);
             fprintf("Last control loop took %.2f seconds\n", deltaTime);
             motor.measurementClock = tic;
@@ -72,29 +73,30 @@ classdef Motor < handle
 
             if (motor.movementMode == 1)
                 % Update the angle PID
-                motor.anglePID.updateErrorState(motor.currentAngle - motor.angleTarget, deltaTime);
+                motor.ANGLE_PID.updateErrorState(motor.currentAngle - motor.angleTarget, deltaTime);
 
             elseif (motor.movementMode == 2)
                 % Update the velocity PID
-                motor.velocityPID.updateErrorState(motor.currentVelocity - motor.velocityTarget, deltaTime);
+                motor.VELOCITY_PID.updateErrorState(motor.currentVelocity - motor.velocityTarget, deltaTime);
             end
         end
 
         function ManageSetTargets(motor)
             % Manages the currently set targets so that they are met
-            %   Will ignore the angle target if the angle target is []
-            %   and the velocity target is []
+            arguments (Input)
+                motor Motor % This Motor object
+            end
 
             % Get the current motor data values
             motor.UpdateData();
 
             if (motor.movementMode == 1)
                 % Manage the motor angle
-                controlOutput = -motor.anglePID.calculateControlOutput();
+                controlOutput = -motor.ANGLE_PID.calculateControlOutput();
                 
             elseif (motor.movementMode == 2)
                 % Manage the motor velocity 
-                controlOutput = -motor.velocityPID.calculateControlOutput();
+                controlOutput = -motor.VELOCITY_PID.calculateControlOutput();
                 
             else
                 controlOutput = 0;
@@ -104,21 +106,34 @@ classdef Motor < handle
         end
 
         function Stop(motor, brake)
+            % Stops this motor
+            arguments (Input)
+                motor Motor % This Motor object
+                brake string = "coast" % Should the motor break to stop
+            end
             motor.ClearTargets();
             motor.brick.StopMotor(motor.port, brake);
         end
 
         
-        % Target Management Methods
         function ClearTargets(motor)
+            % Clears this motor's set targets
+            arguments (Input)
+                motor Motor % This Motor object
+            end
             motor.angleTarget = [];
             motor.velocityTarget = [];
             motor.movementMode = 0;
-            motor.anglePID.reset();
-            motor.velocityPID.reset();
+            motor.ANGLE_PID.reset();
+            motor.VELOCITY_PID.reset();
         end
 
         function SetAngleTarget(motor, target)
+            % Sets the absolute target angle of this motor
+            arguments (Input)
+                motor Motor   % This Motor object
+                target double % The absolute target angle in degrees
+            end
             motor.ClearTargets();
             motor.UpdateData();
             motor.angleTarget = target + fix(motor.currentAngle / 360) * 360;
@@ -126,6 +141,11 @@ classdef Motor < handle
         end
 
         function SetRelAngleTarget(motor, relTarget)
+            % Sets the relative target angle of this motor
+            arguments (Input)
+                motor Motor   % This Motor object
+                relTarget double % The relative target angle in degrees
+            end
             motor.ClearTargets();
             motor.UpdateData();
             motor.angleTarget = relTarget + motor.currentAngle;
@@ -133,30 +153,68 @@ classdef Motor < handle
         end
 
         function SetVelocityTarget(motor, target)
+            % Sets the target velocity of this motor
+            arguments (Input)
+                motor Motor   % This Motor object
+                target double % The relative target velocity in deg/s
+            end
             motor.ClearTargets();
             motor.velocityTarget = target;
             motor.movementMode = 2;
         end
 
-
-        % Getter Methods
         function angle = GetCurrentAngle(motor)
+            % Gets the last measured angle of the motor
+            arguments (Input)
+                motor Motor % This Motor object
+            end
+            arguments (Output)
+                angle double % The last measured angle of the motor in degrees
+            end
             angle = motor.currentAngle;
         end
 
         function angleTarget = GetCurrentAngleTarget(motor)
+            % Gets the target angle of the motor
+            arguments (Input)
+                motor Motor % This Motor object
+            end
+            arguments (Output)
+                angleTarget double % The target angle of the motor in degrees
+            end
             angleTarget = motor.angleTarget;
         end
 
         function angularVelocity = GetCurrentVelocity(motor)
+            % Gets the last measured angular velocity of the motor
+            arguments (Input)
+                motor Motor % This Motor object
+            end
+            arguments (Output)
+                angularVelocity double % The last measured velocity of the motor in deg/s
+            end
             angularVelocity = motor.currentVelocity;
         end
 
         function angularVelocityTarget = GetCurrentVelocityTarget(motor)
+            % Gets the target velocity of the motor
+            arguments (Input)
+                motor Motor % This Motor object
+            end
+            arguments (Output)
+                angularVelocityTarget double % The target angular velocity of the motor in deg/s
+            end
             angularVelocityTarget = motor.velocityTarget;
         end
 
         function angularAcceleration = GetCurrentAcceleration(motor)
+            % Gets the last measured angular acceleration of the motor
+            arguments (Input)
+                motor Motor % This Motor object
+            end
+            arguments (Output)
+                angularAcceleration double % The last measured acceleration of the motor in deg/s²
+            end
             angularAcceleration = motor.currentAcceleration;
         end
     end
