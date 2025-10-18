9b63633b3839721efbb10244c6f6924f66da1d0b
jonbassoon25 <124705409+jonbassoon25@users.noreply.github.com>
2025-10-18 10:05:54 -0700

    Create RangeFinder.m

---
+++ MATLAB/HardwareInterfaces/RangeFinder.m
@@ -0,0 +1,24 @@
+classdef RangeFinder
+    % Controls the RangeFinder of the vehicle
+
+    properties (Access = public)
+        motor Motor
+        ultraSensor UltrasonicSensor
+        targetMotorVelocity double
+          
+    end
+
+    methods
+        function obj = RangeFinder(inputArg1,inputArg2)
+            %RANGEFINDER Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
